7148777742604e468d01709da1a3174162927115
unknown <elsukovdenis8@gmail.com>
2026-03-12 18:03:50 +0300

    feat: create validation

--- script.js
+++ script.js
@@ -1,53 +1,138 @@
-const formElement = document.querySelector('form')
-const loginElement = formElement.elements['login'];
-const passwordElement = formElement.elements['password'];
-
-const errorElement = document.createElement('div');
-const seePassElement = formElement.elements['check-password'];
-errorElement.className = 'error-text';
-errorElement.textContent = 'Пример: user@example.com'
-
-
-formElement.addEventListener('submit', (event) => {
-  event.preventDefault();
-})
-
-formElement.addEventListener('change',(event) => {
-  if (event.target === loginElement) {
-    const regExp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
-    if (!regExp.test(event.target.value)) {
-      loginElement.classList.add('error')
-      loginElement.after(errorElement)
-    } else {
-      loginElement.classList.remove('error')
-      errorElement.remove()
-      loginElement.classList.add('success')
+class FormsValidation {
+    selectors = {
+      form: '[data-js-form]',
+      fieldErrors: '[data-js-form-field-errors]',
+      checkPassword: '[data-js-form-check-password]',
+      password: '[data-js-form-password]',
     }
-  }
 
-  if (event.target === passwordElement) {
-    let len = event.target.value.length;
-    console.log(passwordElement.value.length);
-    if (! (8 <= len <= 20)) {
-      passwordElement.classList.add('error')
-      errorElement.textContent = 'Пароль от 8 до 20 символов';
-      passwordElement.after(errorElement)
-    } else {
-      passwordElement.classList.remove('error')
-      errorElement.remove()
-      passwordElement.classList.add('success')
+    errorMessages = {
+      valueMissing: () => 'Пожалуйста, заполните это поле',
+      patternMismatch: () => 'Данные не соответствуют формату',
+      tooShort: ({ minLength }) => `Слишком короткое значение. Минимальное значение символов - ${minLength}.`,
+      tooLong: ({ maxLength }) => `Слишком длинное значение. Максимальное значение символов - ${maxLength}.`,
     }
-  }
-})
 
-
-formElement.addEventListener('click', (event) => {
-  console.log(event.target)
-  if ( event.target === seePassElement ) {
-    if ( seePassElement.checked) {
-      passwordElement.type = 'text';
-    } else {
-      passwordElement.setAttribute('type', 'password');
+    constructor() {
+      this.bindEvents()
     }
-  }
-})
+
+
+    manageErrors(fieldControlElement, errorMessages) {
+      const fieldErrorsElement = fieldControlElement.parentElement.querySelector(this.selectors.fieldErrors)
+      fieldErrorsElement
+        .innerHTML = errorMessages
+        .map((message) => `<span class="field__error">${message}</span>`).join('')
+    }
+
+
+    validateField(fieldControlElement) {
+      const errors = fieldControlElement.validity
+      const errorMessages = []
+
+      Object.entries(this.errorMessages).forEach(([errorType, getErrorMessage]) => {
+        if (errors[errorType]) {
+          errorMessages.push(getErrorMessage(fieldControlElement))
+        }
+      })
+
+      this.manageErrors(fieldControlElement, errorMessages)
+
+      const isValid = errorMessages.length === 0
+
+      fieldControlElement.ariaInvalid = !isValid
+
+      return isValid
+    }
+
+    onBlur(event) {
+      const { target } = event
+
+      const isFormField = target.closest(this.selectors.form)
+      const isRequired = target.required
+
+      if (isFormField && isRequired) {
+        this.validateField(target)
+      }
+    }
+
+    onChange(event) {
+      const { target } = event
+      const isRequired = target.required
+      const isToggleType = ['radio', 'checkbox'].includes(target.type)
+
+      if ( isRequired && isToggleType ) {
+        this.validateField(target)
+      }
+    }
+
+    onSubmit(event) {
+
+      const isFormElement = event.target.matches(this.selectors.form)
+
+      if (!isFormElement) {
+        return
+      }
+
+      const requiredControlElements = [... event.target.elements]
+        .filter(({ required }) => required === true)
+
+      let isFormValid = true
+
+      let firstInvalidFieldContol = null
+
+      requiredControlElements.forEach((element) => {
+        let isFiledValid = this.validateField(element)
+
+        this.validateField(element)
+
+        if (!isFiledValid) {
+          isFormValid = false
+
+          if (!firstInvalidFieldContol) {
+            firstInvalidFieldContol = element
+          }
+        }
+
+        if (!isFormValid) {
+          event.preventDefault()
+          firstInvalidFieldContol.focus()
+        }
+      })
+    }
+
+    onWatchPassword(event) {
+      const isCheckWatch = event.target.matches('[data-js-form-check-password]')
+      const checkElement = document.querySelector('[data-js-form-check-password]')
+      const passwordElement = document.querySelector('[data-js-form-password]')
+      if (isCheckWatch) {
+        if (checkElement.checked) {
+          passwordElement.setAttribute('type', 'text')
+        } else {
+          passwordElement.setAttribute('type', 'password')
+        }
+      }
+    }
+
+
+    bindEvents() {
+      document.addEventListener('blur', (event) => {
+        this.onBlur(event)
+      }, {capture: true})
+
+      document.addEventListener('change', (event) => {
+        this.onChange(event)
+      })
+
+      document.addEventListener('submit', (event) => {
+        this.onSubmit(event)
+      })
+
+      document.addEventListener('click', (event) => {
+        this.onWatchPassword(event)
+      })
+    }
+}
+
+
+new FormsValidation()
