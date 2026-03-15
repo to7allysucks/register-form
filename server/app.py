a4276d49a1e5692a80d9ac3b367a9764c82c3642
glitchcore <to7allysucks@gmail.com>
2026-03-15 16:09:01 +0300

    feat: implement registration request logic

--- server/app.py
+++ server/app.py
@@ -1,19 +1,51 @@
 import sqlite3
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)
 CORS(app)
 
+def init_db():
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT NOT NULL,
+            email TEXT NOT NULL UNIQUE,
+            password TEXT NOT NULL
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
+init_db()
+
 @app.route('/register', methods=['POST'])
 def register():
     data = request.json
+    username = data.get('username')
+    email = data.get('email')
+    password = data.get('password')
     print(f"Полученные данные: {data}")
 
-    return jsonify({
-        "status": "success",
-        "message": "Данные получены."
-    }), 200
+    if not username or not email or not password:
+        return jsonify({"error": "Заполните все поля!"}), 400
+
+    hashed_password = generate_password_hash(password)
+
+    try:
+        conn = sqlite3.connect('users.db')
+        cursor = conn.cursor()
+
+        cursor.execute('INSERT INTO users (username,email, password) VALUES (?, ?, ?)', (username, email, hashed_password))
+        conn.commit()
+        conn.close()
+        return jsonify({"message": f"Успешная регистрация!"}), 201
+
+    except sqlite3.IntegrityError:
+        return jsonify({"error": "Такой email уже зарегистрирован"}), 400
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
